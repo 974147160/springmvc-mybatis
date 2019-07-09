c3f5fb0e5f929e724ef68bab218dab7ae3b45f74
974147160 <$ 974147160@qq.com>
2019-07-09 16:39:53 +0800

    commit all files

---
+++ src/main/java/com/ysh/model/YshCar.java
@@ -0,0 +1,12 @@
+package com.ysh.model;
+
+
+import java.io.Serializable;
+
+public class YshCar implements Serializable {
+    //alt + enter serialVersionUID
+    private static final long serialVersionUID = 993681197870146751L;
+    private String carNo;
+    private String carType;
+    private String carColor;
+}
---
+++ src/main/java/com/ysh/model/YshHouse.java
@@ -0,0 +1,11 @@
+package com.ysh.model;
+
+import java.io.Serializable;
+
+/**
+ * @Author: jyliang
+ * @Date: 2019/7/9 16:38
+ */
+public class YshHouse implements Serializable {
+    private static final long serialVersionUID = 6737215599585038982L;
+}
