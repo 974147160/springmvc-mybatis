93db938632c9c2a40058ac2a55767d0c7a4cb9c9
974147160 <$ 974147160@qq.com>
2019-07-10 14:25:04 +0800

    commit all files

---
+++ src/main/java/com/ysh/model/YshFood.java
@@ -0,0 +1,11 @@
+package com.ysh.model;
+
+import java.io.Serializable;
+
+/**
+ * @Author: jyliang
+ * @Date: 2019/7/10 14:14
+ */
+public class YshFood implements Serializable {
+    private static final long serialVersionUID = 1050655440800220509L;
+}
